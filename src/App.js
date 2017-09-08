bb15ca025c87e8be3883bbaaea8c194856d5b38e
sforsell <mskforsell@gmail.com>
2017-09-08 16:38:23 -0400

    add font awesome icons, and can stop and start an audiostream

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FaMicrophone from 'react-icons/lib/fa/microphone';
+import FaMicrophoneSlash from 'react-icons/lib/fa/microphone-slash';
 
+  // const handleSuccess = function(stream) {
+  //   window.localStream = stream;
+  //   var audio = document.querySelector('audio');
+    
+  //   if ("srcObject" in audio) {
+  //     audio.srcObject = stream;
+  //   } else {
+  //     audio.src = window.URL.createObjectURL(stream);
+  //   }
+  //   audio.onloadedmetadata = function(e) {
+  //     audio.play();
+  //   };
+  //   // if (window.URL) {
+  //   //   player.src = window.URL.createObjectURL(stream);
+  //   // } else {
+  //   //   player.src = stream;
+  //   // }
+  // };
+
+  // poss outdated?
+  // navigator.getUserMedia  = navigator.getUserMedia ||
+  //                           navigator.webkitGetUserMedia ||
+  //                           navigator.mozGetUserMedia ||
+  //                           navigator.msGetUserMedia;
+  
 class App extends Component {
+  constructor(){
+    super()
+    this.shouldStop = this.shouldStop.bind(this);
+    this.record = this.record.bind(this);
+  }
+  
+  record = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then(stream => {
+        window.localStream = stream;
+        var audio = document.querySelector('audio');
+  
+          if ("srcObject" in audio) {
+            audio.srcObject = stream;
+          } else {
+            audio.src = window.URL.createObjectURL(stream);
+          }
+
+          audio.onloadedmetadata = function(e) {
+            audio.play();
+          };
+      })
+      .catch(err => {
+      console.log(err);
+    });
+
+  };
+
+  shouldStop(){
+    window.localStream.getAudioTracks()[0].stop();
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to OmniLingual</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="textbox">
+          text goes here
+        </div>
+        <button onClick={() => {this.record()}} className="startRecording"> <FaMicrophone /> </button>
+        <button onClick={() => {this.shouldStop()}} className="stopRecording"> <FaMicrophoneSlash /> </button>
+        <audio > </audio>
       </div>
     );
   }
